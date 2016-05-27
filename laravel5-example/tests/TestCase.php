670053536046a2713ed731c28f8c82e5858065d5
Tùng Tờ Tờ <tttung@outlook.com>
2016-05-27 23:49:44 +0700

    Revert "Revert "sua ghi chu //tungdemo""
    This reverts commit 0308ec16df06e5593c691f1ff7f459ef75b2c041.

--- laravel5-example/tests/TestCase.php
+++ laravel5-example/tests/TestCase.php
@@ -12,7 +12,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 		$app = require __DIR__.'/../bootstrap/app.php';
 
 		$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
+		//tungdemo
 		return $app;
 	}
 
